7a61ea5dada45bce34061deeec6e6759f3f5bac6
Nikita <nteplitskiy@TX150>
2020-04-01 02:09:47 -0400

    initial frequency compensation attempt

--- SimpleCDMA/MsgDecode.m
+++ SimpleCDMA/MsgDecode.m
@@ -1,3 +1,5 @@
+clear all; close all; clc
+
 load('Rcvd_Teplitskiy.mat');
 PN = [8 7 6 1 0];
 B_RCOS = [0.0038 0.0052 -0.0044 -0.0121 -0.0023 0.0143 0.0044 -0.0385 ...
@@ -9,18 +11,56 @@ B_RCOS = [0.0038 0.0052 -0.0044 -0.0121 -0.0023 0.0143 0.0044 -0.0385 ...
 PNGEN = comm.PNSequence('Polynomial', PN, 'InitialConditions', 1, ...
     'SamplesPerFrame', 2^8-1, 'Mask', de2bi(1,8));
 PNSEQ = PNGEN();
+bpPN = rectpulse(pskmod(PNSEQ, 2), 4);
 
 oPN = rectpulse(PNSEQ, 4);
 
 rx = filter(B_RCOS, 1, Rcvd);
 
-pcorr = correlate(oPN, rx');
-
-plot([1:1020], pcorr);
-
+pcorr = correlate(oPN, real(rx'));
+%pcorr = correlate(rectpulse(bpPN,4), rx');
+figure; plot([1:1020], pcorr);
+figure; plot(real(xcorr(oPN, rx')));
 [max,ind] = max(pcorr)
 
+%%
+
+%plot(abs(fftshift(fft(rx))));
+
+
+%%
+
+
+
+rxPN = circshift(oPN, ind);
+
+pcorr = correlate(rxPN, rx');
+%pcorr = correlate(rectpulse(bpPN,4), rx');
+
+figure; plot([1:1020], real(pcorr));
+figure; plot(real(xcorr(rxPN, rx')));
+
+figure;
+scatterplot(rx);
+
 hadamard(8)
+
+rxDEPN = repmat(circshift(bpPN, ind), length(rx)/length(bpPN), 1)' .* rx;
+
+%%
+close all;
+f = 31 * 2 * pi;
+t = linspace(0, length(Rcvd)/1e6, length(Rcvd));
+s = sin(f * t + 1.25*pi);
+
+figure; plot(real(xcorr(rxPN, rx')));
+hold on;
+plot(s*420);
+hold off;
+
+new = filter(B_RCOS, 1, Rcvd .* s);
+figure; plot(real(xcorr(rxPN, new')));
+scatterplot(new);
 
 function rate = correlate(ref, test)
     
